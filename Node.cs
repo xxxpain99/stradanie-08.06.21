9cc843a779e2af472a90b306038a65ff46852aff
xxxpain99 <84181647+xxxpain99@users.noreply.github.com>
2021-06-08 14:43:19 +0500

    Add files via upload

---
+++ Node.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp22
+{
+	class Node <T>
+	{
+		public T value;
+		public Node<T> next;
+		public Node<T> prev;
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp22
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MyList<int> ert = new MyList<int>();
+			ert.Add(45);
+			ert.Add(55);
+			ert.Add(65);
+			ert.AddFront(89);
+			ert.Print();
+
+		}
+	}
+}
